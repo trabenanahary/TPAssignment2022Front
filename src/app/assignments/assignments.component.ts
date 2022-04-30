187d9bff6123dcf90087f34002cd2ba2dff049ff
trabenanahary <trabenanahary@gmail.com>
2022-05-01 00:47:28 +0300

    Gestion de la pagination

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -12,6 +12,16 @@ import { Assignment } from './assignment.model';
 export class AssignmentsComponent implements OnInit {
   assignments: Assignment[] = [];
   dialogAssignment!: MatDialogRef <AssignmentDetailComponent>
+   // pagination
+   page=1;
+   limit=10;
+   totalPages=0;
+   pagingCounter=0;
+   hasPrevPage=false;
+   hasNextPage=true;
+   prevPage= 1;
+   nextPage= 2;
+ 
   constructor(private assignmentsService:AssignmentsService, private dialog: MatDialog) {}
 
   openDetailsDialog(assign: Assignment) {
@@ -19,21 +29,53 @@ export class AssignmentsComponent implements OnInit {
       data: assign
     })
     this.dialogAssignment.afterClosed().subscribe(result => {
-      this.assignmentsService.getAssignmentsComplete().subscribe((assignments: any) => {
-        this.assignments = assignments;
-      })
+
+      this.getAssignments();
+
     })
   }
 
   // appelé après le constructeur et AVANT l'affichage du composant
   ngOnInit(): void {
-    console.log("Dans ngOnInit, appelé avant l'affichage")
-    // demander les données au service de gestion des assignments...
-    this.assignmentsService.getAssignmentsComplete()
-    .subscribe((assignments: any) => {
-      this.assignments = assignments;
-    });
+    console.log("Dans ngOnInit, appelé avant l'affichage");
+    this.getAssignments();
+  }
 
-    console.log("Après l'appel au service");
+  getAssignments() {
+      // demander les données au service de gestion des assignments...
+      this.assignmentsService.getAssignments( this.page, this.limit)
+      .subscribe(reponse => {
+        console.log("données arrivées");
+        this.assignments = reponse.docs;
+        this.page = reponse.page;
+        this.limit=reponse.limit;
+        this.totalPages=reponse.totalPages;
+        this.pagingCounter=reponse.pagingCounter;
+        this.hasPrevPage=reponse.hasPrevPage;
+        this.hasNextPage=reponse.hasNextPage;
+        this.prevPage= reponse.prevPage;
+        this.nextPage= reponse.nextPage;
+     });
+
+      console.log("Après l'appel au service");
+  }
+  pagePrecedente() {
+    this.page--;
+    this.getAssignments();
+  }
+
+  pageSuivante() {
+    this.page++;
+    this.getAssignments();
+  }
+
+  premierePage() {
+    this.page = 1;
+    this.getAssignments();
+  }
+
+  dernierePage() {
+    this.page = this.totalPages;
+    this.getAssignments();
   }
 }
--- src/app/shared/assignments.service.ts
+++ src/app/shared/assignments.service.ts
@@ -13,99 +13,18 @@ import { bdInitialAssignments } from './data';
 export class AssignmentsService {
   private localProtocol = "http"
   private localPort = ":8010"
-  //private url = (document.domain.startsWith("localhost") ? this.localProtocol : "https")+"://"+ document.domain + (document.domain.startsWith("localhost") ? this.localPort : "")+"/api"
-  private url = "https://tpmbdsmadagascarback.herokuapp.com/api";
-  /*assignments:Assignment[] = [
-    {
-      id:1,
-      auteur: "Micka",
-      matiere: "Angular",
-      titre:"Angular",
-      nom:"Mr Polo",
-      notes:0,
-      remarques: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      dateDeRendu: new Date("2022-03-28"),
-      rendu:false
-    },
-    {
-      id:2,
-      auteur: "Rahiza rahiza Micka",
-      matiere: "Oracle",
-      titre:"Oracle",
-      nom: 'Mr Polo',
-      notes:10,
-      remarques: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      dateDeRendu: new Date("2022-01-22"),
-      rendu:true
-    },
-    {
-      id:3,
-      auteur: "Micka",
-      matiere: "Grails",
-      titre: "Grails",
-      nom: 'Mr Polo',
-      notes:0,
-      remarques: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      dateDeRendu: new Date("2022-04-01"),
-      rendu:false
-    },
-    {
-      id:4,
-      auteur: "Rahiza rahiza Micka",
-      matiere: "Oracle",
-      titre:"Oracle",
-      nom: 'Mr Polo',
-      notes:18,
-      remarques: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      dateDeRendu: new Date("2022-01-22"),
-      rendu:true
-    },
-    {
-      id:5,
-      auteur: "Rahiza rahiza Micka",
-      matiere: "Oracle",
-      titre:"Oracle",
-      nom: 'Mr Polo',
-      notes:18,
-      remarques: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      dateDeRendu: new Date("2022-01-22"),
-      rendu:true
-    },
-    {
-      id:6,
-      auteur: "Rahiza rahiza Micka",
-      matiere: "Oracle",
-      titre:"Oracle",
-      nom: 'Mr Polo',
-      notes:14,
-      remarques: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      dateDeRendu: new Date("2022-01-22"),
-      rendu:true
-    },
-    {
-      id:7,
-      auteur: "Micka",
-      matiere: "Oracle",
-      titre:"Oracle",
-      nom: 'Mr Polo',
-      notes:11,
-      remarques: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      dateDeRendu: new Date("2022-01-22"),
-      rendu:true
-    },
-    {
-      id:9,
-      auteur: "Micka",
-      matiere: "Oracle",
-      titre:"Oracle",
-      nom: 'Mr Polo',
-      notes:13,
-      remarques: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      dateDeRendu: new Date("2022-01-22"),
-      rendu:true
-    },
+  private url = (document.domain.startsWith("localhost") ? this.localProtocol : "https")+"://"+ document.domain + (document.domain.startsWith("localhost") ? this.localPort : "")+"/api"
+  //private url = "https://tpmbdsmadagascarback.herokuapp.com/api";
+  
+  getAssignments(page:number, limit:number):Observable<any> {
+    // en réalité, bientôt au lieu de renvoyer un tableau codé en dur,
+    // on va envoyer une requête à un Web Service sur le cloud, qui mettra un
+    // certain temps à répondre. On va donc préparer le terrain en renvoyant
+    // non pas directement les données, mais en renvoyant un objet "Observable"
+    //return of(this.assignments);
+    return this.http.get<Assignment[]>(this.url + "/assignments?page=" + page + "&limit=" + limit);
+  }
 
-];*/
 
   constructor(private loggingService:LoggingService, private http: HttpClient) {
     this.loggingService.setNiveauTrace(2);
